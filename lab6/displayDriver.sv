10413b2deb9dc04027ffa79f57d50f84a40b2034
Brendan Weibel <BMWeibel@gmail.com>
2020-06-01 11:16:21 -0700

    started working directory for lab 6. Started graphics implementation

---
+++ lab6/displayDriver.sv
@@ -0,0 +1,34 @@
+module displayDriver(dataIn, clock, VGA_R, VGA_G, VGA_B, VGA_BLANK_N, VGA_CLK, VGA_HS, VGA_SYNC_N, VGA_VS);
+	input logic [39:0] dataIn [9:0];
+	input logic clock;
+	
+	output [7:0] VGA_R;
+	output [7:0] VGA_G;
+	output [7:0] VGA_B;
+	output VGA_BLANK_N;
+	output VGA_CLK;
+	output VGA_HS;
+	output VGA_SYNC_N;
+	output VGA_VS;
+	
+	
+	VGA_framebuffer fb (
+		.clk50			(clock), 
+		.reset			(1'b0), 
+		.x(finalX), 
+		.y(finalY),
+		.pixel_color(pixel_color && KEY[1]), 
+		.pixel_write	(1'b1),
+		.VGA_R, 
+		.VGA_G, 
+		.VGA_B, 
+		.VGA_CLK, 
+		.VGA_HS, 
+		.VGA_VS,
+		.VGA_BLANK_n	(VGA_BLANK_N), 
+		.VGA_SYNC_n		(VGA_SYNC_N));
+		
+		
+	
+endmodule
+
