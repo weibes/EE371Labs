d99ce0ea380ca26657c2b99ba66550a8dc57bcca
ashiqulkazi <55562692+ashiqulkazi@users.noreply.github.com>
2020-06-10 06:13:20 -0700

    comments

--- lab6/playfield.sv
+++ lab6/playfield.sv
@@ -1,3 +1,22 @@
+/* A playfield module that serves as the essential game logic design module of the system
+ * and that handles the user interface and the internal game interface for the system.
+ * 
+ * Inputs:
+ *   Clock - the synchronous clock signal.
+ *   reset - the reset state for the FSMs and used to syncronyze with the clock.
+ *   motion_enable - this is the signal that is coming from the top level module to assert, whether 
+ *                   the user is able to press key to start moving pieces in the system.
+ *	  motion - this is a 2 bit number that allows the user to use the PS/2 keyboard arrow keys to move
+ *            and to assert the specific rotations of the block pieces.
+ *   address_b - this a 5 bit number that serves as the inport port from the RAM that is used for the graphics display
+ *					  to be used to display the game logic design in the graphics.
+ *   rden_b - this is the read enable signal for the graphics driver to use on whether to allow data to be read.
+ *							
+ *
+ * Outputs:
+ * 	q_b - this is a 12 bit number that serves as the output port for the graphics module to utlize each of the rows
+ *          that is contained in the display monitor.
+ */
 module playfield(Clock, reset, motion_enable, motion, address_b, 
 						rden_b, q_b);
 	input logic Clock, reset, motion_enable;
@@ -6,6 +25,8 @@ module playfield(Clock, reset, motion_enable, motion, address_b,
 	input logic rden_b; 
 	output logic [11:0] q_b;
 	
+	// Declare logic statements and variables. Connect the internal ports
+	// with respective input and output ports.
 	logic [4:0] next_x, loc_x, next_y, loc_y, do_collision, no_collision;
 	logic [4:0] piece[5:0];
 	logic [4:0] next_piece[5:0];
@@ -18,17 +39,19 @@ module playfield(Clock, reset, motion_enable, motion, address_b,
 	logic rden_a, wren_a, down_request;
 	logic piece_request, piece_ready;
 	logic [4:0] newpiece[5:0];
-	
-	//logic piece_is_moving;
+	logic piece_is_moving;
 	
 	assign collide = ((q_a & decode_out[0:11]) > 12'b0);
 	
+	// instantiates the two port storage ram for storing both the game logic designs and the display for graphics design.
+	// instantiates the decoder that is being used for the collision detection FSM.
+	// instantiates the piece_generate module that is being used to generate the pieces 
 	pof_RAM storage(.address_a, .address_b, .clock(Clock), .data_a, .data_b(11'b0), 
 						 .rden_a, .rden_b, .wren_a, .wren_b(1'b0), .q_a, .q_b);
 	decoder_4_to_16 decode(.in(decode_in), .out(decode_out));
 	piece_generate pieces (.Clock, .reset, .request(piece_request), .arr_out(newpiece), .data_ready(piece_ready));
 	
-	// the internal FSM for controlling the playfield.
+	// the internal FSM for controlling the playfield and the user I/O interface.
 	enum {waiting, down, left, right, cw, ccw} ps, ns;
 	always_comb begin
 	integer i;
@@ -96,7 +119,7 @@ module playfield(Clock, reset, motion_enable, motion, address_b,
 					ns = waiting;
 			end
 		endcase
-	end
+	end //always_comb
 	
 	always_ff @(posedge Clock) begin
 		if(reset)
@@ -108,20 +131,19 @@ module playfield(Clock, reset, motion_enable, motion, address_b,
 			else
 				piece <= piece;
 		end
-	end
+	end //always_ff
 	
 	// piece_is_moving detects when a piece is moving, for logic for piece moving graphically
-	//assign piece_is_moving = (ps == down);        // we added this to replace the motion_enable
+	assign piece_is_moving = (ps == down);  
 	
-	
-	// FSM for detecting collisions and moving the pieces
+	// FSM for detecting collisions and clearing the rows, when a collision had been detected.
 	enum {waiting_col, clear0, clear1, clear2, clear3, 
 			check0, check1, check2, check3, write0, write1, write2, write3} ps_col, ns_col;
 	
 	always_comb begin
 	//default:
 		address_a = 5'bX;
-		data_a = 12'b0;               // we set data_a to default to 0
+		data_a = 12'b0;             
 		decode_in = 4'b0;
 		wren_a = 1'b0;
 		rden_a = 1'b1;
@@ -263,7 +285,7 @@ module playfield(Clock, reset, motion_enable, motion, address_b,
 					if(counter == 2'b11) 
 						ns_col = write0;	
 					else
-						ns_col = check3;            // this one change we made to compile
+						ns_col = check3;         
 				end
 				write0:
 				begin
@@ -334,7 +356,7 @@ module playfield(Clock, reset, motion_enable, motion, address_b,
 						ns_col = write3;	
 				end
 		endcase
-	end
+	end //always_comb
 	
 	always_ff @(posedge Clock) begin
 		if(reset) begin
@@ -357,10 +379,10 @@ module playfield(Clock, reset, motion_enable, motion, address_b,
 			else
 				no_collision <= no_collision;
 		end
-	end
+	end //always_ff
 	
-	// FSM for creating and moving the pieces on the screen
-	enum {create, waiting_create, move_down} ps_create, ns_create;
+	// FSM for creating and moving the pieces on the screen, which connects the other two FSM for user interface and collision detect.
+	enum {create, waiting_create, moving_down_delay, move_down} ps_create, ns_create;
 	always_comb begin
 	// default:
 	piece_request = 1'b0;
@@ -375,25 +397,25 @@ module playfield(Clock, reset, motion_enable, motion, address_b,
 				else
 					ns_create = create;
 				end
-			waiting_create:        //changes made starting from here
-				begin   
-					//if(motion_enable) begin
-						create_nextcounter = create_counter + 24'b000000000000000000001;
-						if(create_counter == 24'b0)
-							ns_create = move_down;
-						else
-							ns_create = waiting_create;
-					//end
+			waiting_create:       
+				begin      
+				if (ns_col == waiting_col)
+					ns_create = create;
+				else begin
+					if(piece_is_moving)
+						ns_create = moving_down_delay;
+					else
+						ns_create = waiting_create; 				
 				end
-		//	moving_down_delay:
-	//			begin
-				// debug line, remove for actual
-				//if(create_counter == 24'b0)
-			//	if (create_counter == 24'h000005)
-			//		ns_create = move_down;	
-			//	else
-			//		ns_create = moving_down_delay;
-		//		end                          // until here
+				end
+			moving_down_delay:
+				begin
+				create_nextcounter = create_counter + 24'b000000000000000000001;
+				if (create_counter == 24'h000005)
+					ns_create = move_down;	
+				else
+					ns_create = moving_down_delay;
+				end                        
 			move_down:
 				begin
 				create_nextcounter = 1;
@@ -404,27 +426,30 @@ module playfield(Clock, reset, motion_enable, motion, address_b,
 					ns_create = move_down;
 				end
 		endcase
-	end
+	end //always_comb
 	
 	always_ff @(posedge Clock) begin
 		if(reset) begin
 			ps_create <= create;
-			create_counter <= 24'b0;            // added this
-			loc_x <= 0;                         // added this
-			loc_y <= 0;                         // added this
+			create_counter <= 24'b0;         
+			loc_x <= 0;                       
+			loc_y <= 0;                       
 		end
 		else begin  
-			ps_create <= ns_create;             // added these
+			ps_create <= ns_create;					
 			loc_x <= next_x;
 			loc_y <= next_y;
 			create_counter <= create_nextcounter;
 		end
-	end
-endmodule
+	end //always_ff
+	
+endmodule //playfield
 
+// timescaling the testbench to synch with the library memory module.
 `timescale 1 ps / 1 ps
+/* testbench for the playfield that tests the overall function of the module and outputs 
+   produced given the varied inputs. Port connections *see playfield module*. */
 module playfield_testbench();
-
 	logic Clock, reset, motion_enable;
 	logic [1:0] motion;
 	logic [4:0] address_b;
@@ -445,7 +470,7 @@ module playfield_testbench();
 		reset = 1;	rden_b = 1;	motion = 2'b00;	@(posedge Clock);
 		reset = 0;	motion_enable = 0;				@(posedge Clock);
 		for (i = 0; i < 1000; i++) begin
-			@(posedge Clock);
+																@(posedge Clock);
 			if (addressRead == 19)
 				addressRead = 0;
 			else
