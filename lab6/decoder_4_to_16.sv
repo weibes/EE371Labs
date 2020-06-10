550b0594f43cc7c6d6fccf53c13c369c8ce4fb55
ashiqulkazi <55562692+ashiqulkazi@users.noreply.github.com>
2020-06-10 06:17:55 -0700

    comments

--- lab6/decoder_4_to_16.sv
+++ lab6/decoder_4_to_16.sv
@@ -1,3 +1,12 @@
+/* A decoder_4_to_16 module is used within the playfield module to decode a 4 to 16 bit
+ * number within the collision detection FSM that is being asserted within the playfield.
+ *
+ * Inputs:
+ * 	in - a 4 bit input for the decoder that represents the coordinate locations being written using the RAM.
+ *
+ * Outputs:
+ * 	out - the 16 bit output from the decoder that represents the RAM data signals of the game logic.
+ */
 module decoder_4_to_16(in, out);
 	input logic [3:0] in;
 	output logic [0:15] out;
@@ -7,6 +16,7 @@ module decoder_4_to_16(in, out);
 		for(i = 0; i < 16; i++) begin
 			out[i] = (in == i);
 		end
-	end
+	end //always_comb
 	
-endmodule
+endmodule // decoder_4_to_16
+
